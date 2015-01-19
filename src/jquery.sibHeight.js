aa0cb317c26a5539a0292dc90e695ff962d85e6e
Steve Norell <design@stevenorell.com>
2015-01-19 09:12:24 -0700

    Added default breakpoint.

--- src/jquery.sibHeight.js
+++ src/jquery.sibHeight.js
@@ -1,20 +1,9 @@
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
 ;(function ( $, window, document, undefined ) {
-
-// undefined is used here as the undefined global variable in ECMAScript 3 is
-// mutable (ie. it can be changed by someone else). undefined isn't really being
-// passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-// can no longer be modified.
-
-// window and document are passed through as local variable rather than global
-// as this (slightly) quickens the resolution process and can be more efficiently
-// minified (especially when both are regularly referenced in your plugin).
 
 // Create the defaults once
 var pluginName = "sibHeight",
 	defaults = {
-		propertyName: "value"
+		breakPoint: "768px"
 	};
 
 // The actual plugin constructor
@@ -55,8 +44,6 @@ Plugin.prototype = {
 	}
 };
 
-// A really lightweight plugin wrapper around the constructor,
-// preventing against multiple instantiations
 $.fn[ pluginName ] = function ( options ) {
 	return this.each(function() {
 		if ( !$.data( this, "plugin_" + pluginName ) ) {
